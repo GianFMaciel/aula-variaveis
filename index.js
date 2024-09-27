b0600387e30ee85e72a95a8915dd0396dff648dd
Gian F Maciel <gian.f.maciel@gmail.com>
2024-09-26 21:46:00 -0300

    Commit da aula de variaveis com JavaScript

---
+++ index.js
@@ -0,0 +1,10 @@
+//apenas um comentario de linha unica
+
+/*apenas um comentario
+multi linha*/
+
+let nome = prompt('Qual é o seu nome?')
+
+alert('Seja Bem Vindo ' + nome + ' !')
+
+console.log('variavel' nome)
