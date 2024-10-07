98a2cf2a389713c361d0827b0bdbf7a5ecb936c4
Gian F Maciel <gian.f.maciel@gmail.com>
2024-10-07 19:35:45 -0300

    commit

--- exercicioescrita1.js
+++ exercicioescrita1.js
@@ -1,4 +1,5 @@
 let nome = prompt('Qual é seu nome ?')
 let idade = prompt('Qual é sua idade ?')
 
-console.log(typeof nome, typeof idade) //underfined underfined por as duas variaveis não conter valores como strings ou number
+console.log(typeof nome, typeof idade) //Resposta exercicio D) underfined underfined por as duas variaveis não conter valores como strings ou number
+                                       // Após atribuir prompt será exibido os valoler que o usuário atribuir.
